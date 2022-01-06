03b05766b67cb14450440c23a1b26b6004d72a6c
H20 Studio <89566553+hendarofficialyt@users.noreply.github.com>
2022-01-06 18:36:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-apt upgrade
-apt update
-pkg update
-pkg upgrade
-pkg install php
-pkg install python
+pkg install neofetch
 pkg install curl
-pkg install requests
-pkg install nano
+pip install -r requirements.txt
